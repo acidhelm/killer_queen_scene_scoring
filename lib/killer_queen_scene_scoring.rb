314bb49970a15a9188289c5d2a0c88dbdf28bbb7
Michael Dunn <acidhelm@gmail.com>
2019-03-13 19:00:52 -0700

    Fix dependencies and require lines

--- lib/killer_queen_scene_scoring.rb
+++ lib/killer_queen_scene_scoring.rb
@@ -1,6 +1,15 @@
+require "json"
+require "rest-client"
+require "killer_queen_scene_scoring/bracket.rb"
+require "killer_queen_scene_scoring/config"
+require "killer_queen_scene_scoring/match"
+require "killer_queen_scene_scoring/player"
+require "killer_queen_scene_scoring/scene"
+require "killer_queen_scene_scoring/team"
+require "killer_queen_scene_scoring/tournament"
 require "killer_queen_scene_scoring/version"
 
 module KillerQueenSceneScoring
-  class Error < StandardError; end
-  # Your code goes here...
+    class Error < StandardError
+    end
 end
