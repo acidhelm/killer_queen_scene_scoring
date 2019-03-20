0286c8d0486987dfedb22e70fa285bec5177d962
Michael Dunn <acidhelm@gmail.com>
2019-03-19 20:34:43 -0700

    Get ready to use the VCR gem

--- killer_queen_scene_scoring.gemspec
+++ killer_queen_scene_scoring.gemspec
@@ -37,6 +37,8 @@ Gem::Specification.new do |spec|
     spec.add_development_dependency "bundler", "~> 1.17"
     spec.add_development_dependency "minitest", "~> 5.0"
     spec.add_development_dependency "rake", "~> 10.0"
+    spec.add_development_dependency "vcr", "~> 4.0"
+    spec.add_development_dependency "webmock", "~> 3.5.1"
 
     spec.add_runtime_dependency "dotenv", "~> 2.7"
     spec.add_runtime_dependency "json", "~> 2.2"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,14 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "killer_queen_scene_scoring"
-
+require "vcr"
 require "minitest/autorun"
+
+VCR.configure do |config|
+    config.cassette_library_dir = "test/vcr_cassettes"
+    config.debug_logger = File.new("log/vcr_test.log", "a")
+    config.ignore_localhost = true
+    config.default_cassette_options = {
+        record: :new_episodes,
+        re_record_interval: 5 * 60 * 60 * 24 }  # 5 days
+    config.hook_into :webmock
+end
