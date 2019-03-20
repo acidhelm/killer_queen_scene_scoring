6b1b45d681daef3eaa8a28d69d946b712b0d04cf
Michael Dunn <acidhelm@gmail.com>
2019-03-19 21:40:17 -0700

    Only tournament.load needs to be in a cassette

--- test/killer_queen_scene_scoring_test.rb
+++ test/killer_queen_scene_scoring_test.rb
@@ -7,18 +7,17 @@ class KillerQueenSceneScoringTest < Minitest::Test
 
     def test_a_6_team_tournament
         api_key = ENV["CHALLONGE_API_KEY"]
-        tournament = nil
 
         if !api_key
             flunk "You must set the CHALLONGE_API_KEY variable in your environment" \
                     " or the .env file to your API key."
         end
 
-        VCR.use_cassette("calc_scores_tvtpeasf") do
-            tournament = KillerQueenSceneScoring::Tournament.new(
-                           id: "tvtpeasf", api_key: api_key,
-                           logger: Logger.new("log/test.log"))
+        tournament = KillerQueenSceneScoring::Tournament.new(
+                       id: "tvtpeasf", api_key: api_key,
+                       logger: Logger.new("log/test.log"))
 
+        VCR.use_cassette("calc_scores_tvtpeasf") do
             assert tournament.load
         end
 
