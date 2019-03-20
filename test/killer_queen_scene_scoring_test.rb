99ee3cf2e28f441c7434a8b666a1123d12cf0a5e
Michael Dunn <acidhelm@gmail.com>
2019-03-19 20:51:12 -0700

    Add a simple test that loads a Tournament

--- test/killer_queen_scene_scoring_test.rb
+++ test/killer_queen_scene_scoring_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class KillerQueenSceneScoringTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::KillerQueenSceneScoring::VERSION
-  end
+    def test_the_version_number
+        refute_nil KillerQueenSceneScoring::VERSION
+    end
+
+    def test_a_6_team_tournament
+        api_key = ENV["CHALLONGE_API_KEY"]
+        tournament = nil
+
+        if !api_key
+            flunk "You must set the CHALLONGE_API_KEY variable in your environment" \
+                    " or the .env file to your API key."
+        end
+
+        VCR.use_cassette("calc_scores_tvtpeasf") do
+            tournament = KillerQueenSceneScoring::Tournament.new(
+                           id: "tvtpeasf", api_key: api_key,
+                           logger: Logger.new("log/test.log"))
+
+            assert tournament.load
+        end
+
+        # TODO: Check the scene and score names.
+        tournament.calculate_points
+    end
 end
