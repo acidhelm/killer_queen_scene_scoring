2bd9e154f8d383697b38c497d052617e4c266655
Michael Dunn <acidhelm@gmail.com>
2019-03-21 12:04:05 -0700

    Test score calculation for the two demo tourneys and bb3

--- test/killer_queen_scene_scoring_test.rb
+++ test/killer_queen_scene_scoring_test.rb
@@ -5,23 +5,62 @@ class KillerQueenSceneScoringTest < Minitest::Test
         refute_nil KillerQueenSceneScoring::VERSION
     end
 
-    def test_a_6_team_tournament
-        api_key = ENV["CHALLONGE_API_KEY"]
-
-        if !api_key
+    def verify_scores(slug, expected_scores)
+        unless (api_key = ENV["CHALLONGE_API_KEY"])
             flunk "You must set the CHALLONGE_API_KEY variable in your environment" \
                     " or the .env file to your API key."
         end
 
         tournament = KillerQueenSceneScoring::Tournament.new(
-                       id: "tvtpeasf", api_key: api_key,
+                       id: slug, api_key: api_key,
                        logger: Logger.new("log/test.log"))
 
-        VCR.use_cassette("calc_scores_tvtpeasf") do
+        VCR.use_cassette("verify_scores_#{slug}") do
             assert tournament.load
         end
 
-        # TODO: Check the scene and score names.
         tournament.calculate_points
+
+        tournament.scene_scores.sort.each_with_index do |score, i|
+            assert_equal expected_scores[i][0], score.name
+            assert_equal expected_scores[i][1], score.score
+        end
+    end
+
+    def test_a_4_team_tournament
+        expected_scores = [ [ "San Francisco", 12.0 ], [ "Charlotte", 6.0 ],
+                            [ "Chicago", 6.0 ], [ "Minneapolis", 6.0 ],
+                            [ "New York", 5.0 ], [ "Kansas City", 4.0 ],
+                            [ "Portland", 1.0 ], [ "Seattle", 1.0 ] ]
+
+        verify_scores("tvtpeasf", expected_scores)
+    end
+
+    def test_the_kq25_tournament
+        expected_scores = [ [ "New York", 391.5 ], [ "Chicago", 370.5 ],
+                            [ "Charlotte", 316.5 ], [ "Minneapolis", 292.0 ],
+                            [ "Portland", 275.0 ], [ "Columbus", 219.5 ],
+                            [ "Kansas City", 160.5 ], [ "Seattle", 88.0 ],
+                            [ "Madison", 73.0 ], [ "Austin", 46.0 ],
+                            [ "San Francisco", 37.0 ], [ "CHA", 21.0 ],
+                            [ "Los Angeles", 15.5 ], [ "Iowa", 11.0 ] ]
+
+        verify_scores("clonekqxxvwc", expected_scores)
+    end
+
+    def test_the_bb3_tournament
+        expected_scores = [ [ "New York", 951.0 ], [ "Chicago", 902.5 ],
+                            [ "Charlotte", 803.5 ], [ "Portland", 782.5 ],
+                            [ "Minneapolis", 735.5 ], [ "San Francisco", 654.5 ],
+                            [ "Seattle", 478.5 ], [ "Columbus", 288.0 ],
+                            [ "Austin", 279.0 ], [ "Kansas City", 249.0 ],
+                            [ "Phoenix", 202.5 ], [ "St. Louis", 132.0 ],
+                            [ "Cincinnati", 90.0 ], [ "Chattanooga", 82.5 ],
+                            [ "Madison", 70.5 ], [ "Los Angeles", 60.0 ],
+                            [ "Atlanta", 55.5 ], [ "South Florida", 45.0 ],
+                            [ "Elkader", 28.5 ], [ "Baltimore", 16.5 ],
+                            [ "Eugene", 13.5 ], [ "Canada", 4.5 ] ]
+
+        verify_scores("bb3wc", expected_scores)
     end
 end
